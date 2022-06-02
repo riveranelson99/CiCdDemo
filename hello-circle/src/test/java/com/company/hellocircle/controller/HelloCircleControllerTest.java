f50cc2ca6ae8589a256f10bc3ec1d0508ec3a4fa
Nelson Rivera <riveranelson99@gmail.com>
2022-06-02 09:43:48 -0500

    Added tests

---
+++ hello-circle/src/test/java/com/company/hellocircle/controller/HelloCircleControllerTest.java
@@ -0,0 +1,33 @@
+package com.company.hellocircle.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HelloCircleController.class)
+public class HelloCircleControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void shouldReturn200AndSayHello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello, Circle!"));
+
+    }
+
+}
